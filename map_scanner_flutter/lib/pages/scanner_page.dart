eeb8be435f44e934037e105a0f7d8b66baa2ac54
Adrien Hellec <adrien.hellec@epita.fr>
2019-03-09 18:40:03 +0100

    flutter: add openfoodfact api: product getter

--- map_scanner_flutter/lib/pages/scanner_page.dart
+++ map_scanner_flutter/lib/pages/scanner_page.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../dbo/product.dart';
 
 class ScannerPage extends StatefulWidget {
   @override
@@ -14,6 +18,7 @@ class ScannerPage extends StatefulWidget {
 class ScannerPageState extends State<ScannerPage> {
 
   String _result = "";
+  Product _product;
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +36,33 @@ class ScannerPageState extends State<ScannerPage> {
             padding: const EdgeInsets.all(8.0),
           ),
           new Text("Barcode number after scan: " + _result),
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+          ),
+          _product != null ? new Text("Product Name: " + _product.name) : (_result != "" ? new Text("Product not found") : new Container()),
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+          ),
+          _product != null ? new Text("Product Brand: " + _product.brand) : new Container(),
         ],
       ),
     );
+  }
+
+  Future<Product> getProductFromBarcode(String barcode) async {
+    final response = await http.get("https://fr.openfoodfacts.org/api/v0/produit/$barcode.json");
+    try {
+      var jsonResponse = json.decode(response.body);
+      print("BARCODE: " + barcode);
+      if (jsonResponse["status_verbose"] != "product found" ||
+          jsonResponse["product"]["product_name"] == null ||
+          jsonResponse["product"]["brands"] == null)
+        return null;
+      return new Product(jsonResponse["product"]["product_name"], barcode, jsonResponse["product"]["brands"]);
+    } catch (ex) {
+      print(ex);
+    }
+    return null;
   }
 
   Future _scanQR() async {
@@ -42,6 +71,9 @@ class ScannerPageState extends State<ScannerPage> {
       this.setState(() {
         _result = qrResult;
       });
+      getProductFromBarcode(_result).then((product) => this.setState(() {
+        _product = product;
+      }));
     } on PlatformException catch (ex) {
       if (ex.code ==BarcodeScanner.CameraAccessDenied) {
         this.setState(() {
