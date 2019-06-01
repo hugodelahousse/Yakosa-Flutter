05e848b5771538975da93aa2dd9557e2db2617ee
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-06-01 19:09:16 +0200

    Screen: Add Profile with GraphQL PoC

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:yakosa/screens/profile_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:yakosa/utils/api.dart';
 
 import './screens/login_page.dart';
 import './screens/home_page.dart';
@@ -12,11 +16,32 @@ class Routes {
   };
 
   Routes() {
+
+    final HttpLink httpLink = HttpLink(uri: '${Api.baseUrl}/graphql');
+    final AuthLink authLink = AuthLink(
+      getToken: () async {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        return '';
+      },
+    );
+
+    final Link link = authLink.concat(httpLink as Link);
+
+    final ValueNotifier<GraphQLClient> client = ValueNotifier(
+        GraphQLClient(
+          cache: InMemoryCache(),
+          link: link,
+        )
+    );
+
     Auth.isTokenDefined().then((signedIn) {
-      runApp(MaterialApp(
-        title: "Yakosa",
-        home: signedIn ? HomePage() : LoginPage(),
-        routes: routes,
+      runApp(GraphQLProvider(
+          client: client,
+          child: MaterialApp(
+            title: "Yakosa",
+            home: ProfilePage(),
+            routes: routes,
+          )
       ));
     });
   }
