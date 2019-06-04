cf86548076cf1bf5fa49503b0415cc614d96c43f
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-06-04 15:06:20 +0200

    Shopping List Page: Split component

--- lib/screens/shopping_list.dart
+++ lib/screens/shopping_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:yakosa/components/shopping_list/bottom_sheet.dart';
 import 'package:yakosa/components/shopping_list/product_tile.dart';
 import 'package:yakosa/components/shopping_list/quantity_picker.dart';
 import 'package:yakosa/models/product.dart';
@@ -14,13 +15,6 @@ class ShoppingListPage extends StatelessWidget {
     @required this.shoppingListId,
   });
 
-  static const updateQuantityMutation = r"""
-  mutation updateQuantity($id: ID!, $quantity: Int!) {
-    updateListProduct(id: $id, quantity: $quantity) {
-      id
-    }
-  }
-  """;
 
   static const fetchShoppingList = r"""
   query ShoppingList($id: ID!){
@@ -54,41 +48,21 @@ class ShoppingListPage extends StatelessWidget {
             if (result.loading) {
               return Center(child: CircularProgressIndicator());
             }
-
             List products = result.data['shoppingList']['products'];
-
             return ListView.separated(
-                separatorBuilder: (context, index) => const Divider(),
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  final product = ListProduct.fromJson(products[index]);
-                  return Mutation(
-                      options: MutationOptions(document: updateQuantityMutation),
-                      onCompleted: (resultData) {
-                        // Close bottom sheet and refetch data
-                        Navigator.pop(context);
-                        refetch();
-                      },
-                      builder: (updateQuantity, result) {
-                        return ProductTile(
-                          product: product,
-                          onPressed: () => showModalBottomSheet(
-                            context: context,
-                            builder: (context) => QuantityPicker(
-                              product: product,
-                              onUpdateQuantity: (quantity) {
-                                if (quantity == product.quantity) {
-                                  // No update needed, close the bottom sheet
-                                  Navigator.pop(context);
-                                  return;
-                                }
-                                updateQuantity({'id': product.id, 'quantity': quantity});
-                              }
-                            ),
-                          ),
-                        );
-                  });
-                }
+              separatorBuilder: (context, index) => const Divider(),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = ListProduct.fromJson(products[index]);
+                return ProductTile(
+                  product: product,
+                  onPressed: () => showModalBottomSheet(
+                    context: context,
+                    builder: (context) =>
+                        ShoppingListBottomSheet(product, refetch),
+                  ),
+                );
+              },
             );
           },
         )
