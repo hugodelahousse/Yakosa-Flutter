7f59fb49c362a7287bfbb65e4e57e5ae58a6dea0
Adrien Hellec <adrien.hellec@epita.fr>
2019-06-13 18:38:45 +0200

    map: google maps & fetch stores via graphql & custom icons

--- lib/screens/promotions_map_page.dart
+++ lib/screens/promotions_map_page.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
-import 'package:location/location.dart';
+import 'package:flutter/services.dart';
+import 'package:location/location.dart' as location;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:yakosa/utils/graphql.dart';
 
 class PromotionsMapPage extends StatefulWidget {
 @override
@@ -11,95 +18,109 @@ class PromotionsMapPage extends StatefulWidget {
 }
 
 class PromotionsMapPageState extends State<PromotionsMapPage> {
-  var location = new Location();
-  LatLng userLocation = LatLng(48.853188, 2.349213);
+  GoogleMapController _controller;
+  var userLocation = LatLng(48.853188, 2.349213);
+
+  var _location = location.Location();
+
+  Map<String, Marker> markers = {};
+  BitmapDescriptor mapIcon;
+
+  static const fetchNearbyStores = r"""
+    query NearbyStores($position: String!, $distance: String!, $limit: Int!){
+      nearbyStore(
+        position: $position,
+        distance: $distance,
+        limit: $limit
+      ){
+        id,
+        position{
+          coordinates
+        },
+        brand{
+          name
+        }
+      }
+    }
+    """;
+
+  Future<Uint8List> getBytesFromAsset(String path) async {
+    ByteData data = await rootBundle.load(path);
+    Codec codec = await instantiateImageCodec(data.buffer.asUint8List());
+    FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ImageByteFormat.png)).buffer.asUint8List();
+  }
 
   void initState() {
     super.initState();
-    location.getLocation().then((x) {
+    _location.getLocation().then((value) {
       setState(() {
-        userLocation.latitude = x.latitude;
-        userLocation.longitude = x.longitude;
+        userLocation = LatLng(value.latitude, value.longitude);
       });
     });
+    if (mapIcon == null) {
+      getBytesFromAsset('assets/images/map-icon/3.0x/map-icon.png').then((asset) {
+        mapIcon = BitmapDescriptor.fromBytes(asset);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-     options: MapOptions(
-       center: userLocation,
-       zoom: 13.0,
-     ) ,
-     layers: [
-       TileLayerOptions(
-         urlTemplate: "https://api.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
-         additionalOptions: {
-           'accessToken': 'pk.eyJ1IjoiYWRyaWVuaGVsbGVjIiwiYSI6ImNpbDl1ZDkxdjAwMDd3cW0yZnpkbmJrd2gifQ.DMnVXVlm_M3tgwa86S-VpA',
-           'id': 'mapbox.streets',
-           },
-       ),
-       MarkerLayerOptions(
-         markers: [
-           Marker(
-             width: 40.0,
-             height: 40.0,
-             point:  LatLng(48.853188, 2.349213),
-             builder: (ctx) =>
-             Container(
-               decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5.0)]
-               ),
-               child: IconTheme(data: IconThemeData(color: Colors.purple), child: new Icon(Icons.store)),
-             )
-           ),
-           Marker(
-             width: 40.0,
-             height: 40.0,
-             point:  LatLng(48.822372, 2.352175),
-             builder: (ctx) =>
-             Container(
-               decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5.0)]
-               ),
-               child: IconTheme(data: IconThemeData(color: Colors.purple), child: new Icon(Icons.store)),
-             )
-           ),
-           Marker(
-             width: 40.0,
-             height: 40.0,
-             point:  LatLng(48.813074, 2.361432),
-             builder: (ctx) =>
-             Container(
-               decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5.0)]
-               ),
-               child: IconTheme(data: IconThemeData(color: Colors.purple), child: new Icon(Icons.store)),
-             )
-           ),
-           Marker(
-             width: 40.0,
-             height: 40.0,
-             point:  LatLng(48.823832, 2.361737),
-             builder: (ctx) =>
-             Container(
-               decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5.0)]
-               ),
-               child: IconTheme(data: IconThemeData(color: Colors.purple), child: new Icon(Icons.store)),
-             )
-           )
-         ]
-       )
-     ]
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: userLocation,
+        zoom: 14),
+      onMapCreated: _onMapCreated,
+      onCameraMove: _onCameraMove,
+      compassEnabled: true,
+      myLocationButtonEnabled: true,
+      myLocationEnabled: true,
+      markers: Set<Marker>.of(markers.values),
     );
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _controller = controller;
+    });
+    //fetchStores(userLocation.longitude, userLocation.latitude, "10000", 100);
+  }
+
+  _onCameraMove(CameraPosition position) {
+    fetchStores(position.target.longitude, position.target.latitude, "3000", 100);
+  }
+
+  _onTapMarker(String id) {
+    print(id + " TAPPED");
+  }
+
+  fetchStores(double long, double lat, String dist, int limit) {
+    graphQLCLient.value.query(
+      QueryOptions(
+        document: fetchNearbyStores,
+        variables: {
+          "position": "{\"type\": \"Point\", \"coordinates\": [$long, $lat]}",
+          "distance": dist,
+          "limit": limit,
+        },
+      ),
+    ).then((result) {
+      if (result.errors == null && result.data != null) {
+        List stores = result.data['nearbyStore'];
+        var newMarkers = Map<String, Marker>();
+        stores.forEach((s) {
+          var marker = Marker(
+            markerId: MarkerId(s['id']),
+            position: LatLng(s['position']['coordinates'][1], s['position']['coordinates'][0]),
+            icon: mapIcon,
+            onTap: () => _onTapMarker(s['id']));
+          newMarkers.putIfAbsent(s['id'], () => marker);
+        });
+        setState(() {
+            markers = newMarkers;
+        });
+      }
+    });
   }
 }
