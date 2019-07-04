6b2c79ab8687f2c61b4544c9008e5c8566533804
Adrien Hellec <adrien.hellec@epita.fr>
2019-07-04 19:30:00 +0200

    shopping_list: update page & product tile design

--- lib/components/shopping_list/product_tile.dart
+++ lib/components/shopping_list/product_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:yakosa/models/product.dart';
@@ -13,21 +15,52 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new GestureDetector(
+    return GestureDetector(
       onTap: onPressed,
-        child: ListTile(
-          leading: Image.network(
-            product.product.info.image_url,
-            width: 100,
-            height: 100,
-            fit: BoxFit.fitHeight,
-          ),
-          title: Text(
-            product.product.info.product_name_fr,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          trailing: Text('${product.quantity}x'),
+      child: Container(
+        height: 140,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              height: 100,
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(product.product.info.image_url),),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [BoxShadow(color: Color(0x40000000), blurRadius: 5, spreadRadius: 2)],
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.7),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Text(product.quantity.toString(), style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white)),
+                  )
+                )
+              )
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width * 0.7,
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [BoxShadow(color: Color(0x40000000), blurRadius: 5, spreadRadius: 2)],
+                ),
+                child: Center(child: Text(product.product.info.product_name_fr, style: TextStyle(fontSize: 15))),
+              ),
+            )
+          ],
         )
+      ),
     );
   }
 }
